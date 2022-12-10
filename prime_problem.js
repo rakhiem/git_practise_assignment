e0fa660d1610c0ac84c32d18707ab06b76dfd2c0
Your Name <you@example.com>
2022-12-10 22:06:53 +0530

    m2_code_fix_checked

--- prime_problem.js
+++ prime_problem.js
@@ -1,19 +1,13 @@
-let num=13;
- let check=true;
-for(let i=2;i<num;i++)
-{
-if(num%i==0)
-{
-check =false;
-break;
-}
-}
+ let num=13;
+ let count=0;
 
-if(check==true)
-{
-console.log("It is Prime.");
+for (let i=1; i<=num; i++){
+  if (num%i==0){
+    count++;
+  }
 }
-else
-{
-console.log("It is not prime");
+if (count==2){
+  console.log ("Yes");
+} else {
+  console.log ("No");
 }
